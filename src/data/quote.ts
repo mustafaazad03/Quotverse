86b393f2062cca1ef976d54db622fb36bc920ddf
Mustafa Azad <mustafaazad533@gmail.com>
2024-03-15 15:05:21 +0530

    Seperate data file build

---
+++ src/data/quote.ts
@@ -0,0 +1,26 @@
+export const QuoteData = [
+  {
+    id: 1,
+    quote: "Forgiveness is choosing to love. It is the first skill of self-giving love.",
+    author: "Mahatma Gandhi",
+    category: "wisdom",
+    subCategory: "love"
+  },
+  {
+    id: 2,
+    quote: "A youth, when at home, should be filial and, abroad, respectful to his elders. He should be earnest and truthful. He should overflow in love to all and cultivate the friendship of the good. When he has time and opportunity, after the performance of these things, he should employ them in polite studies.",
+    author: "Confucius",
+    category: "wisdom",
+    subCategory: "love"
+  },
+  {
+    id: 3,
+    quote: "Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited",
+    author: "aura",
+  },
+  {
+    id: 4,
+    quote: "Hold onto your seats, folks! The new iPhone is coming in hot, ready to rock our world! Imagine the stunning photos and videos I'll capture with this beauty! 📸💥 #BringItOnNewiPhone #BeyondExcited",
+    author: "aura"
+  },
+]
